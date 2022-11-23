916b067c795eb3c7db745c95ebc59314a4a5dda7
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-23 19:09:00 -0300

    Commit - Inserindo pastas

---
+++ SlnSistema/src/SistemaCadastro.Models/Model/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCadastro.Models.Model
+{
+    public class Pessoa
+    {
+        public Int32 Id { get; set; }
+
+    }
+}
